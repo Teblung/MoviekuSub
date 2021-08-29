290ce0cad0e69461e879a80e2ecd7fbd80d9abfb
Teblung <theo.sepdianto97@gmail.com>
2021-08-29 21:54:02 +0700

    Add certification pinning

--- core/src/main/java/com/teblung/movieku/core/di/CoreModule.kt
+++ core/src/main/java/com/teblung/movieku/core/di/CoreModule.kt
@@ -11,6 +11,7 @@ import com.teblung.movieku.core.domain.repository.IMovieRepository
 import com.teblung.movieku.core.utils.AppExecutors
 import net.sqlcipher.database.SQLiteDatabase
 import net.sqlcipher.database.SupportFactory
+import okhttp3.CertificatePinner
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
@@ -26,8 +27,8 @@ val databaseModule = module {
         val passphrase: ByteArray = SQLiteDatabase.getBytes("teblung".toCharArray())
         val factory = SupportFactory(passphrase)
         Room.databaseBuilder(
-                androidContext(),
-                MovieDatabase::class.java, "Movie.db"
+            androidContext(),
+            MovieDatabase::class.java, "Movie.db"
         ).fallbackToDestructiveMigration()
             .openHelperFactory(factory)
             .build()
@@ -36,30 +37,39 @@ val databaseModule = module {
 
 val networkModule = module {
     single {
+        val hostname = "api.themoviedb.org"
+        val certificatePinner = CertificatePinner.Builder()
+            .add(hostname, "sha256/+vqZVAzTqUP8BGkfl88yU7SQ3C8J2uNEa55B7RZjEg0=")
+            .add(hostname, "sha256/JSMzqOOrtyOT1kmau6zKhgT676hGgczD5VMdRMyJZFA=")
+            .add(hostname, "sha256/++MBgDH5WGvL9Bcn5Be30cRcL0f5O+NyoXuWtQdX1aI=")
+            .add(hostname, "sha256/KwccWaCgrnaw6tsrrSO61FgLacNgG2MMLq8GE6+oP5I=")
+            .build()
         OkHttpClient.Builder()
-                .addInterceptor(
-                        Interceptor { chain ->
+            .addInterceptor(
+                Interceptor { chain ->
 
-                            val original = chain.request()
-                            val originalHttpUrl = original.url
-                            val url = originalHttpUrl.newBuilder()
-                                    .addQueryParameter("api_key", BuildConfig.KEY)
-                                    .build()
-                            val requestBuilder = original.newBuilder().url(url)
-                            val request = requestBuilder.build()
+                    val original = chain.request()
+                    val originalHttpUrl = original.url
+                    val url = originalHttpUrl.newBuilder()
+                        .addQueryParameter("api_key", BuildConfig.KEY)
+                        .build()
+                    val requestBuilder = original.newBuilder().url(url)
+                    val request = requestBuilder.build()
 
-                            chain.proceed(request)
-                        }).addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
-                .connectTimeout(120, TimeUnit.SECONDS)
-                .readTimeout(120, TimeUnit.SECONDS)
-                .build()
+                    chain.proceed(request)
+                })
+            .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+            .connectTimeout(120, TimeUnit.SECONDS)
+            .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
+            .build()
     }
     single {
         val retrofit = Retrofit.Builder()
-                .baseUrl("https://api.themoviedb.org/3/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .client(get())
-                .build()
+            .baseUrl("https://api.themoviedb.org/3/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(get())
+            .build()
         retrofit.create(ApiService::class.java)
     }
 }
@@ -70,9 +80,9 @@ val repositoryModule = module {
     factory { AppExecutors() }
     single<IMovieRepository> {
         MoviekuRepository(
-                get(),
-                get(),
-                get()
+            get(),
+            get(),
+            get()
         )
     }
 }
